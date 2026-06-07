f16141364869c37dfc2ffe90a439440d509717ec
caiobraga <caiotagb@gmail.com>
2026-06-07 09:01:12 -0300

    fix backend release error

--- src/usecases/aiText.ts
+++ src/usecases/aiText.ts
@@ -1,6 +1,7 @@
 import type { Request } from "express";
 import type { AppConfig } from "../config.js";
 import type { SupabaseGateway } from "../infra/gateways.js";
+import type { AuthUseCases } from "./auth.js";
 import { buildProposalFieldAiUseCases, type ProposalFieldAiUseCases } from "./proposalFieldAi.js";
 
 export type AiTextUseCases = ProposalFieldAiUseCases;
